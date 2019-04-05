457e30d5c5b10aaff2da83b276e628dc6d3d0889
NandaMochammad <nanda.mocha@gmail.com>
2019-04-05 15:17:11 +0700

    Initial Commit

---
+++ CobaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CobaGit
+//
+//  Created by Nanda Mochammad on 05/04/19.
+//  Copyright © 2019 Nanda Mochammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
